5a85d3bc35300f45434286a9eb283696c6cc18b2
Facu00H <faisher00@gmail.com>
2022-11-07 18:42:07 -0300

    first commit

---
+++ routes/index.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var router = express.Router();
+
+
+router.get("/", (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+router.get("/api/", (req, res) => {
+  res.status(200).json({
+    utc: `${new Date().toString()}`,
+  });
+});
+
+router.get("/api/:date", (req, res) => {
+  const param = req.params['date'];
+  const todayDate = new Date(param);
+  const unixTimestamp = Math.floor(todayDate.getTime() / 1000);
+  if (isNaN(Date.parse(todayDate)) === false) {
+    res.status(200).json({
+      unix: unixTimestamp,
+      date: `${todayDate.toString()}`,
+    });
+  }else{
+    const unixToUtc = param * 1000;
+    let unix = Number(param);
+    let dateObj = new Date(unixToUtc);
+    if (isNaN(Date.parse(dateObj)) === false) {
+      res.status(200).json({
+        unix: unix,
+        date: `${dateObj.toString()}`,
+      });
+    }else{
+      res.status(400).json({
+        error: "invalid date",
+      });
+    };
+  };
+});
+
+module.exports = router;
